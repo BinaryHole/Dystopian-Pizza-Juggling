ccfd87e8164d8cd0f6add3609e0144bd18ad0c5e
joshywashys <JoshuaStrutt@cunet.carleton.ca>
2020-02-28 11:31:04 -0500

    started player control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(0, moveVertical, moveHorizontal);
+        rb.AddForce(movement * speed);
+    }
+}
+
+//https://learn.unity.com/tutorial/movement-basics?projectId=5c514956edbc2a002069467c#5c7f8528edbc2a002053b70f move tutorial
+//character will launch in the x direction and will control movement in the y (uo) and z (left/right) directions
