28dc5b2b8ee0c86b3c69afca828d58399de51572
joshywashys <JoshuaStrutt@cunet.carleton.ca>
2020-03-05 14:32:03 -0500

    new

--- Assets/Scripts/LaunchPlayer.cs
+++ Assets/Scripts/LaunchPlayer.cs
@@ -7,7 +7,9 @@ public class LaunchPlayer : MonoBehaviour
     public GameObject player;
     private Rigidbody rb;
     public GameObject mainCamera;
-    public Vector3 launchVector;
+
+    public Vector3 launchVector = new Vector3(0, 30, 0);
+    public Vector3 playerSpeed = new Vector3(0, 0, 1);
     private bool isLaunched;
 
     void Start()
@@ -21,9 +23,14 @@ public class LaunchPlayer : MonoBehaviour
         if (!isLaunched && Input.GetKeyDown(KeyCode.Space))
         {
             isLaunched = true;
-            print("chicken");
-            rb.AddForce(0, 30, 30, ForceMode.Impulse);
+            rb.AddForce(0, 30, 0, ForceMode.Impulse); //switch to use launchvector
             //add a line of code in the camera following player code to check if isLaunched is checked
+        }
+
+        if (isLaunched)
+        {
+            print("hi");
+            rb.transform.position += playerSpeed;
         }
     }
 }
