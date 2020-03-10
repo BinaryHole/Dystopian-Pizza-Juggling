ee0a9ef984da8dbf8700ed136d273fd68d4bb223
Mark McGregor <markmcgregor.contact@gmail.com>
2020-03-10 12:09:07 -0400

    Add pizza throwing
    Lots of vector math and some red debug rays. It works!
    
    The pizza direction is calculated by firing a ray from the camera to the mouse, and detecting when it collides with an object. That point in 3d space is triangulated with the position of the player.

--- Assets/Scripts/InteractionController.cs
+++ Assets/Scripts/InteractionController.cs
@@ -8,6 +8,8 @@ public class InteractionController : MonoBehaviour
     public GameObject hawaiianPizza;
     public GameObject spicyPizza;
 
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,32 @@ public class InteractionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // do player pizza throwing
+        checkForShoot();
+    }
+
+    // Handles pizza throwing (checks if user is pressing left-mouse)
+    void checkForShoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            // instantiate the projectile pizza
+            GameObject pizza = Instantiate(pepperoniPizza, player.transform.position, Quaternion.identity) as GameObject;
+
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Vector3 direction;
+
+            if (Physics.Raycast(cameraRay, out hit))
+            {
+                direction = (hit.point - pizza.transform.position).normalized;
+            } else
+            {
+                direction = cameraRay.direction.normalized;
+            }
+
+            Debug.DrawRay(pizza.transform.position, direction*1000, Color.red, 2);
+            pizza.GetComponent<Rigidbody>().AddForce(direction * 1000);
+        }
     }
 }
