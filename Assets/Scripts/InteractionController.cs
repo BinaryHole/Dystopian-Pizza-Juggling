c0e6d584d38f18cefdcdd445eb61d2821cff3481
Mark McGregor <markmcgregor.contact@gmail.com>
2020-03-10 11:04:48 -0400

    Add interaction controller and placeholder pizzas

---
+++ Assets/Scripts/InteractionController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    public GameObject pepperoniPizza;
+    public GameObject hawaiianPizza;
+    public GameObject spicyPizza;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
