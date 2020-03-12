6ebd6e854b950c817cd523d9358e1bef03331681
brandonwhiteimd <60040405+brandonwhiteimd@users.noreply.github.com>
2020-03-11 20:24:08 -0400

    ADD music, sfx, UPDATE launchplayer
    + Added refs and licenses to music and sfx folders
    + Added kaboom sfx
    + Implemented sounds on launch in launchplayer.cs

--- Assets/Scripts/LaunchPlayer.cs
+++ Assets/Scripts/LaunchPlayer.cs
@@ -11,13 +11,22 @@ public class LaunchPlayer : MonoBehaviour
     public Vector3 launchVector = new Vector3(0, 30, 0);
     public Vector3 playerSpeed = new Vector3(0, 0, 1);
     private bool isLaunched;
+
+    // Vars for audio sources, since there are multiple I use an array.
+    public AudioSource[] sounds;
     public AudioSource surfMusic;
+    public AudioSource kaboom;
 
     void Start()
     {
         isLaunched = false;
         rb = player.GetComponent<Rigidbody>();
-        surfMusic = GetComponent<AudioSource>();
+
+        // Get audio sources
+        sounds = GetComponents<AudioSource>();
+        surfMusic = sounds[0];
+        kaboom = sounds[1];
+
     }
 
     void Update()
@@ -28,7 +37,9 @@ public class LaunchPlayer : MonoBehaviour
             rb.AddForce(0, 30, 0, ForceMode.Impulse); //switch to use launchvector
             //add a line of code in the camera following player code to check if isLaunched is checked
 
+            // Now play music
             surfMusic.Play(0);
+            kaboom.Play(0);
         }
 
         if (isLaunched)
