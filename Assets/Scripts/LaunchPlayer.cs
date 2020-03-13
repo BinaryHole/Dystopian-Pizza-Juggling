d617971b17f9115063ad6a7183d07c81d7571e92
joshywashys <joshuastrutt1@gmail.com>
2020-03-13 01:35:02 -0400

    death, hazards, landing after a certain distance

--- Assets/Scripts/LaunchPlayer.cs
+++ Assets/Scripts/LaunchPlayer.cs
@@ -10,7 +10,11 @@ public class LaunchPlayer : MonoBehaviour
 
     public Vector3 launchVector = new Vector3(0, 30, 0);
     public double playerSpeed = 10;
+    private float playerStartX;
+    private float distanceTravelled;
+    public float maxDistance;
     public bool isLaunched;
+    public bool isLanded;
 
     // Vars for audio sources, since there are multiple I use an array.
     public AudioSource[] sounds;
@@ -30,18 +34,28 @@ public class LaunchPlayer : MonoBehaviour
 
     void Update()
     {
-        if (!isLaunched && Input.GetKey(KeyCode.Space))
+        if (!isLaunched && Input.GetKeyDown(KeyCode.Space))
         {
             // launch the player
             launch();
         }
 
-        if (isLaunched)
+        if (isLaunched && !isLanded)
         {
             Vector3 motion = new Vector3(0, 0, (float)(playerSpeed / 100));
 
             // player moves at constant speed forward
             rb.transform.position += motion;
+        }
+
+        distanceTravelled = playerStartX + player.transform.position.z;
+        if ((distanceTravelled >= maxDistance) || isLanded == true)
+        {
+            isLanded = true;
+            rb.useGravity = true;
+            player.GetComponent<PlayerController>().isLaunched = false;
+            surfMusic.Stop();
+            kaboom.Stop();
         }
     }
 
@@ -49,6 +63,7 @@ public class LaunchPlayer : MonoBehaviour
     {
         isLaunched = true;
         rb.AddForce(0, 30, 0, ForceMode.Impulse); //switch to use launchvector
+        playerStartX = player.transform.position.z;
                                                   //add a line of code in the camera following player code to check if isLaunched is checked
 
         // Now play music
