d199d5b1428671b7b3ad5b901c5dba550f72c49f
Mark McGregor <mark.mcgregor.contact@gmail.com>
2020-03-12 20:13:36 -0400

    Add pizza lifespans and despawning on collision

--- Assets/Scripts/DespawnPizza.cs
+++ Assets/Scripts/DespawnPizza.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class DespawnPizza : MonoBehaviour
 {
+    // the amount of time the pizza will exist
+    public double lifespan;
+
+    // track when the pizza was initialized
+    float intTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        intTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if the pizza has expired (passed it's lifespan)
+        if ((Time.time - intTime) > lifespan)
+        {
+            // destroy the pizza
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if the pizza collides with something other than the player
+        if (other.tag != "Player")
+        {
+            // destroy the pizza
+            Destroy(gameObject);
+        }
     }
 }
