734f3b27168b0e95f44e6fbd1acd13a5c11ad373
Mark McGregor <mark.mcgregor.contact@gmail.com>
2020-03-28 17:05:15 -0400

    Add road variations and improve buildings

---
+++ Assets/Scripts/Player/Death.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    private Rigidbody rb;
+    public GameObject gameManager;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Hazard")
+        {
+            print("death comes to all");
+            gameManager.GetComponent<LaunchPlayer>().isLanded = true;
+        }
+    }
+}
