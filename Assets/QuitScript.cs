c30a981834edf5efcfb36a2ce9c91c8b2f848f94
brandonwhiteimd <60040405+brandonwhiteimd@users.noreply.github.com>
2020-03-29 18:28:22 -0400

    ADD QuitScript
    Working out getting buttons going

---
+++ Assets/QuitScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitScript : MonoBehaviour
+{
+    public Button quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        quitButton.onClick.AddListener(quit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void quit()
+    {
+        Application.Quit();
+    }
+}
