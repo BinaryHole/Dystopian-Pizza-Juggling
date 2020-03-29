fb9fd468be7dafe68564bc2069a8aa39cff1f142
brandonwhiteimd <60040405+brandonwhiteimd@users.noreply.github.com>
2020-03-29 09:02:19 -0400

    Update Death.cs
    Added isDead variable

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -6,11 +6,13 @@ public class Death : MonoBehaviour
 {
     private Rigidbody rb;
     public GameObject gameManager;
+    public int isDead = 0;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Hazard")
         {
+            isDead = 1;
             print("death comes to all");
             gameManager.GetComponent<LaunchPlayer>().isLanded = true;
         }
