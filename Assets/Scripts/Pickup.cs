d1bae35fa3804b70aae6e01ea421f71608d18799
brandonwhiteimd <60040405+brandonwhiteimd@users.noreply.github.com>
2020-03-29 09:12:12 -0400

    Update Pickup.cs
    Cash is now in CountScore, cashAdded is a random int between 50 and 150 now

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    // This is here for now, but should be tied to the GameManager later
-    public int cash;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        cash = 0;
+        
     }
 
     // Update is called once per frame
@@ -23,13 +22,14 @@ public class Pickup : MonoBehaviour
     {
         if (other.gameObject.CompareTag("cash"))
         {
-            // This should be semi-random later.
-            int cashAdded = 150;
+            int cashAdded = Random.Range(50, 150);
             
             other.gameObject.SetActive(false);
             Destroy(other.gameObject);
+
             // This is where you would play a pickup sound effect
-            cash += cashAdded;
+
+            GameObject.Find("Player").GetComponent<CountScore>().cash += cashAdded;
         }
     }
 }
