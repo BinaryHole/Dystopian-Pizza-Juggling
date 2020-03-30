5bb788010b4fd61219fd346f96cff9b2c2412886
brandonwhiteimd <60040405+brandonwhiteimd@users.noreply.github.com>
2020-03-30 11:41:45 -0400

    Update Pickup.cs
    Comments, changed range for cash added

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,14 +20,17 @@ public class Pickup : MonoBehaviour
     {
         if (other.gameObject.CompareTag("cash"))
         {
-            int cashAdded = Random.Range(50, 150);
+            int cashAdded = Random.Range(100, 300);
             
             other.gameObject.SetActive(false);
             Destroy(other.gameObject);
 
             // This is where you would play a pickup sound effect
 
+            
+            // Adds to cash total and to todaysCash. tbh I don't know if I need "cash" still
             CountScore.cash += cashAdded;
+            CountScore.todaysCash += cashAdded;
         }
     }
 }
