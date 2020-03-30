c2448c0d50ea258cb0179dab703826388931674c
brandonwhiteimd <60040405+brandonwhiteimd@users.noreply.github.com>
2020-03-30 10:52:53 -0400

    ADD StatsManagement.cs
    Added script that will handle our statistics

---
+++ Assets/Scripts/StatsManagement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsManagement : MonoBehaviour
+{  
+    public Text uEarned;
+    public Text uRent;
+    public Text uTaxes;
+    public Text uHospital;
+    public Text uProfit;
+    public Text uPizza;
+    public Text uPeople;
+    public Text uMoney;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uEarned.text = CountScore.cash.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
