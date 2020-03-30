171b5daa6ad1a08eb2ee257663545fbecaaf503b
brandonwhiteimd <60040405+brandonwhiteimd@users.noreply.github.com>
2020-03-30 10:53:34 -0400

    Update CountScore.cs
    Fixed errors when not on a screen with cashtext

--- Assets/Scripts/CountScore.cs
+++ Assets/Scripts/CountScore.cs
@@ -2,26 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CountScore : MonoBehaviour
 {
-    public int cash = 0;
-    public int pizzasDelivered = 0;
-    public int peopleKilled = 0;
+    static public int cash = 0;
+    static public int pizzasDelivered = 0;
+    static public int peopleKilled = 0;
+    static public int cashUntilWin = 10000;
+    bool isMenu = false;
     
     public Text CashText;
-
 
     // Start is called before the first frame update
     void Start()
     {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
 
+        if (sceneName != "Level1")
+        {
+            isMenu = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        CashText.text = "Cash: $" + cash;
+        
+        if (isMenu == false)
+        {
+            CashText.text = "Cash: $" + cash;
+        }
+        
     }
 
 }
