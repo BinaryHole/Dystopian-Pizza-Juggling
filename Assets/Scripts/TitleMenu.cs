3cd4aeffa1cb80f0f37e0fefad391ddb8b8a2eb9
brandonwhiteimd <60040405+brandonwhiteimd@users.noreply.github.com>
2020-04-02 19:04:57 -0400

    CREATE TitleMenu.cs
    New script to handle title menu

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    // Start music
+    void Start()
+    {
+        
+    }
+
+    // Play button onclick
+    void PlayNewGame()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    // Instructions button onclick
+    void Instructions()
+    {
+        
+    }
+
+    // Credits button onclick
+    void Credits()
+    {
+
+    }
+
+    // Quit button onclick
+    void Quit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+}
