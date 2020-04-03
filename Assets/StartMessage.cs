df5dd124ded045780b3013a5e889c18a0c1d5048
brandonwhiteimd <60040405+brandonwhiteimd@users.noreply.github.com>
2020-04-03 10:04:48 -0400

    ADD StartMessage.cs
    Makes start message disappear when player starts

---
+++ Assets/StartMessage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMessage : MonoBehaviour
+{
+    bool isStarted = false;
+    public GameObject startText;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startText = GameObject.Find("StartText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isStarted == false && Input.GetKey("space"))
+        {
+            startText.SetActive(false);
+            isStarted = true;
+        }
+    }
+}
